596ae743f51034aa8569cf888ab998dd96abc2f0
Emanuel Rodrigues Valentim <ervalentim@gmail.com>
2023-11-20 13:52:58 -0300

    Implementação dos métodos and e or

--- CodeWrite.py
+++ CodeWrite.py
@@ -1,4 +1,5 @@
 class CodeWriter:
+
     def __init__(self, fname):
         self.output = []
         self.module_name = "Main"
@@ -54,50 +55,64 @@ class CodeWriter:
             self.write("@SP")
             self.write("M=M+1")
     
-    def writePop(seg, index):
+    def writePop(self, seg, index):
         if seg in ["static", "temp", "pointer"]:
-            write(f"@SP  // pop {seg} {index}")
-            write("M=M-1")
-            write("A=M")
-            write("D=M")
-            write(f"@{register_name(seg, index)}")
-            write("M=D")
+            self.write(f"@SP  // pop {seg} {index}")
+            self.write("M=M-1")
+            self.write("A=M")
+            self.write("D=M")
+            self.write(f"@{self.registerName(seg, index)}")
+            self.write("M=D")
         else:
-            write(f"@{register_name(seg, 0)}  // pop {seg} {index}")
-            write("D=M")
-            write(f"@{index}")
-            write("D=D+A")
-            write("@R13")
-            write("M=D")
-            write("@SP")
-            write("M=M-1")
-            write("A=M")
-            write("D=M")
-            write("@R13")
-            write("A=M")
-            write("M=D")
+            self.write(f"@{self.registerName(seg, 0)}  // pop {seg} {index}")
+            self.write("D=M")
+            self.write(f"@{index}")
+            self.write("D=D+A")
+            self.write("@R13")
+            self.write("M=D")
+            self.write("@SP")
+            self.write("M=M-1")
+            self.write("A=M")
+            self.write("D=M")
+            self.write("@R13")
+            self.write("A=M")
+            self.write("M=D")
     
-    def writeArithmeticAdd():
-        write("@SP  // add")
-        write("M=M-1")
-        write("A=M")
-        write("D=M")
-        write("A=A-1")
-        write("M=D+M")
+    def writeArithmeticAdd(self):
+        self.write("@SP  // add")
+        self.write("M=M-1")
+        self.write("A=M")
+        self.write("D=M")
+        self.write("A=A-1")
+        self.write("M=D+M")
 
-    def writeArithmeticSub():
-        write("@SP  // sub")
-        write("M=M-1")
-        write("A=M")
-        write("D=M")
-        write("A=A-1")
-        write("M=M-D")
+    def writeArithmeticSub(self):
+        self.write("@SP  // sub")
+        self.write("M=M-1")
+        self.write("A=M")
+        self.write("D=M")
+        self.write("A=A-1")
+        self.write("M=M-D")
 
-    def writeArithmeticNeg():
-        write("@SP  // neg")
-        write("A=M")
-        write("A=A-1")
-        write("M=-M")
+    def writeArithmeticNeg(self):
+        self.write("@SP  // neg")
+        self.write("A=M")
+        self.write("A=A-1")
+        self.write("M=-M")
+
+    def write_arithmetic_and(self):
+        self.write("@SP // and")
+        self.write("AM=M-1")
+        self.write("D=M")
+        self.write("A=A-1")
+        self.write("M=D&M")
+
+    def write_arithmetic_or(self):
+        self.write("@SP // or")
+        self.write("AM=M-1")
+        self.write("D=M")
+        self.write("A=A-1")
+        self.write("M=D|M")
 
     def write(self, s):
         self.output.append(f"{s}\n")
@@ -108,3 +123,13 @@ class CodeWriter:
     def save(self):
         with open(self.output_file_name, "w") as file:
             file.write("".join(self.output))
+
+
+code_writer = CodeWriter()
+code_writer.write_arithmetic_and()
+code_writer.write_arithmetic_or()
+code_writer.write_arithmetic_not()
+code_writer.write_arithmetic_eq()
+code_writer.write_arithmetic_gt()
+code_writer.write_arithmetic_lt()
+print(code_writer.output)
