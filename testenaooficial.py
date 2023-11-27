e8921e293f332f92441d3612a26f173da4945011
Emanuel Rodrigues Valentim <ervalentim@gmail.com>
2023-11-27 16:22:59 -0300

    teste do goto

--- testenaooficial.py
+++ testenaooficial.py
@@ -11,16 +11,41 @@ code_writer = CodeWriter("output.asm")
 
 # print("Teste bem-sucedido!")
 
-code_writer.set_file_name("TestLabel.vm")
+# code_writer.set_file_name("TestLabel.vm")
 
-# Adicione um comando label ao código VM
-label_name = "Loop"
-code_writer.write_label(label_name)
+# # Adicione um comando label ao código VM
+# label_name = "Loop"
+# code_writer.write_label(label_name)
 
-# Verifique se o código gerado contém o rótulo esperado
-expected_output = f"({code_writer.module_name}${label_name})\n"
+# # Verifique se o código gerado contém o rótulo esperado
+# expected_output = f"({code_writer.module_name}${label_name})\n"
+
+# assert code_writer.code_output() == expected_output, "Erro no teste do método writeLabel"
+
+# print("Teste bem-sucedido!")
+
+code_writer.set_file_name("GotoTest.vm")
+code_writer.write_goto("Loop")
+
+expected_output = """@GotoTest.Loop
+0;JMP
+"""
 print(code_writer.code_output())
 print(expected_output)
-assert code_writer.code_output() == expected_output, "Erro no teste do método writeLabel"
+
+assert code_writer.code_output() == expected_output, "O código assembly gerado não corresponde ao esperado."
 
 print("Teste bem-sucedido!")
+
+
+
+
+
+
+
+
+
+
+
+
+
